8a4daf8355f60e7ec0d7ac5bd30937133f3958af
Nikhil05711 <Nikhil@123>
2022-02-08 18:48:12 +0530

    new updates

--- contracts/StakingReward.sol
+++ contracts/StakingReward.sol
@@ -12,7 +12,7 @@ import "./utils/RewardDistributionReceipt.sol";
 contract StakingReward is
     ReentrancyGuard,
     Rijent,
-  //  Ownable,
+    //  Ownable,
     RewardsDistributionRecipient
 {
     using SafeMath for uint256;
@@ -21,8 +21,8 @@ contract StakingReward is
 
     IBEP20 public StakingToken;
     //only fpr testing (100 * (1E9))
-     uint MIN_DEPOSIT = 100;
-     uint MIN_WITHDRAWAL =  100;
+    uint256 MIN_DEPOSIT = 100;
+    uint256 MIN_WITHDRAWAL = 100;
     uint256 private secondsInOneMonth = 30 * 24 * 60 * 60;
     uint256 public rewardGenerateTime;
     uint256 public _totalReward;
@@ -30,12 +30,9 @@ contract StakingReward is
     uint256 private _stakeAmount;
     uint256 private _capitalPerWeek;
     uint256 private coolingPeriod;
+    uint256 private amountforWeeks;
 
     uint256 private _totalsupply;
-
-
-
-        
 
     mapping(address => uint256) public claimableReward;
     mapping(address => uint256) public totalRewardGenerateFor;
@@ -43,8 +40,6 @@ contract StakingReward is
     mapping(address => uint256[]) public lastRewardTime;
     mapping(address => uint256) public stakingTime;
     mapping(address => uint256) public ClaimedTime;
-
-    
 
     /*-----------CONSTRUCTOR--------------*/
 
@@ -77,7 +72,7 @@ contract StakingReward is
         //for how much time we stake our amount
         coolingPeriod = months.mul(secondsInOneMonth);
         _totalsupply = _totalsupply.add(amount);
-        stakingTime[msg.sender] =  block.timestamp;
+        stakingTime[msg.sender] = block.timestamp;
         totalRewardGenerateFor[msg.sender] = coolingPeriod;
         Rijent.transferPrice(msg.sender, address(this), amount);
         userStakedAmount[msg.sender] = amount;
@@ -90,81 +85,93 @@ contract StakingReward is
         );
     }
 
-    // function rewardGeneration(
-    //     uint256 year,
-    //     uint256 percentRate,
-    //     uint256 months
-    // ) public {
-    //      if (ClaimedTime == 0) {
-    //         require(
-    //             (percentRate == 3 && months == 12 && year == 1) ||
-    //                 (percentRate == 4 && months == 24 && year == 2) ||
-    //                 (percentRate == 5 && months == 36 && year == 3) ||
-    //                 (percentRate == 6 && months == 60 && year == 4),
-    //             "Not in plan"
-    //         );
+    function rewardGeneration(
+        uint256 percentRate,
+        uint256 months
+    ) public {
+        
+            require(
+                (percentRate == 3 && months == 12) ||
+                    (percentRate == 4 && months == 24) ||
+                    (percentRate == 5 && months == 36) ||
+                    (percentRate == 6 && months == 60),
+                "Not in plan"
+            );
 
-    //         require(
-    //             claimableReward[msg.sender] <= _totalReward,
-    //             "Insufficient Reward Found"
-    //         );
+            require(
+                claimableReward[msg.sender] <= _totalReward,
+                "Insufficient Reward Found"
+            );
 
-    //         uint256 totalRewardInPercentage = percentRate.mul(months);
-    //         _totalReward = (
-    //             userStakedAmount[msg.sender].mul(totalRewardInPercentage).div(100)
-    //         );
-    //         // can change totalRewardGenerate to userStakedAmount
-    //         totalRewardGenerateFor[msg.sender] = _totalReward;
-    //         // uint256 totalTime = months * secondsInOneMonth;
-    //         _rewardPerSecond = (_totalReward.div(coolingPeriod));
-    //         rewardGenerateTime = block.timestamp - stakingTime;
-    //         claimableReward[msg.sender] = _rewardPerSecond.mul(
-    //             rewardGenerateTime
-    //         );
-    //     } else {
-    //         rewardGenerateTime = claimableReward[msg.sender].div(_rewardPerSecond);
-    //         claimableReward[msg.sender] = _rewardPerSecond.mul(
-    //             rewardGenerateTime
-    //         );
-    //       }
-    //     emit rewardGenerated(
-    //         msg.sender,
-    //         _totalReward,
-    //         _rewardPerSecond,
-    //         rewardGenerateTime
-    //     );
-    // }
+            uint256 totalRewardInPercentage = percentRate.mul(months);
+            _totalReward = (
+                userStakedAmount[msg.sender].mul(totalRewardInPercentage).div(
+                    100
+                )
+            );
+            // can change totalRewardGenerate to userStakedAmount
+            totalRewardGenerateFor[msg.sender] = _totalReward;
+            // uint256 totalTime = months * secondsInOneMonth;
+            _rewardPerSecond = (_totalReward.div(coolingPeriod));
+            if (ClaimedTime[msg.sender] == 0) {
+            rewardGenerateTime = block.timestamp - stakingTime[msg.sender];
+            claimableReward[msg.sender] = _rewardPerSecond.mul(
+                rewardGenerateTime
+            );
+        } else {
+            rewardGenerateTime = block.timestamp - ClaimedTime[msg.sender];
+            claimableReward[msg.sender] = _rewardPerSecond.mul(
+                rewardGenerateTime
+            );
+        }
+        emit rewardGenerated(
+            msg.sender,
+            _totalReward,
+            _rewardPerSecond,
+            rewardGenerateTime
+        );
+    }
 
     function claim(uint256 amount) public {
+        require(_capitalPerWeek <= userStakedAmount[msg.sender],"NO funds found");
+         ClaimedTime[msg.sender] = block.timestamp;
         if (_totalReward > 0) {
-            require(amount >  MIN_WITHDRAWAL,"out of limit");    
+            require(amount > MIN_WITHDRAWAL, "out of limit");
             require(amount <= _totalReward, "No reward left");
-            ClaimedTime[msg.sender] = block.timestamp;
+            // ClaimedTime[msg.sender] = block.timestamp;
             _totalReward = _totalReward.sub(amount);
             claimableReward[msg.sender] = claimableReward[msg.sender].sub(
                 amount
             );
             Rijent.mint(msg.sender, amount);
         } else if (_totalReward == 0) {
-            require(coolingPeriod < block.timestamp,"Can Claim Capital After Cooling period");
-            ClaimedTime[msg.sender] = block.timestamp;
+            require(
+                coolingPeriod < block.timestamp,
+                "Can Claim Capital After Cooling period"
+            );
+           
             Rijent.transferPrice(address(this), msg.sender, _capitalPerWeek);
         } else {
-            uint256 lastClaimedTime = block.timestamp.sub(ClaimedTime[msg.sender]);
+            uint256 lastClaimedTime = block.timestamp.sub(
+                ClaimedTime[msg.sender]
+            );
+            uint256 numberOfWeeks = lastClaimedTime.div(604800);
             require(
-                lastClaimedTime == ClaimedTime[msg.sender].add(604800),
+                lastClaimedTime == ClaimedTime[msg.sender].add(90),
                 "can claim only after 7 days"
             );
-            Rijent.transferPrice(address(this), msg.sender, _capitalPerWeek);
+
+            amountforWeeks = _capitalPerWeek.mul(numberOfWeeks);
+            
+            Rijent.transferPrice(address(this), msg.sender, amountforWeeks);
         }
-            emit claimedReward(
-                msg.sender,
-                amount,
-                _totalReward,
-                _rewardPerSecond,
-                _capitalPerWeek          
-            );
-        
+        emit claimedReward(
+            msg.sender,
+            amount,
+            _totalReward,
+            _rewardPerSecond,
+            amountforWeeks
+        );
     }
 
     // function exit() external {
@@ -184,7 +191,7 @@ contract StakingReward is
         uint256 amount,
         uint256 _totalReward,
         uint256 _claimableReward,
-        uint256 _capitalPerWeek
+        uint256 amountforWeeks
     );
     event rewardDurationUpdated(uint256 newRewardDuration);
     event rewardPaid(address receiver, uint256 amount);
@@ -201,51 +208,4 @@ contract StakingReward is
         uint256 stakeAmount,
         uint256 capitalPerWeek
     );
-
-
- function rewardGeneration(
-        uint256 percentRate,
-        uint256 months
-    ) public {
-        
-            require(
-                (percentRate == 3 && months == 12) ||
-                    (percentRate == 4 && months == 24) ||
-                    (percentRate == 5 && months == 36) ||
-                    (percentRate == 6 && months == 6),
-                "Not in plan"
-            );
-
-            require(
-                claimableReward[msg.sender] <= _totalReward,
-                "Insufficient Reward Found"
-            );
-
-            uint256 totalRewardInPercentage = percentRate.mul(months);
-            _totalReward = (
-                userStakedAmount[msg.sender].mul(totalRewardInPercentage).div(100)
-            );
-            // can change totalRewardGenerate to userStakedAmount
-            totalRewardGenerateFor[msg.sender] = _totalReward;
-            // uint256 totalTime = months * secondsInOneMonth;
-            _rewardPerSecond = (_totalReward.div(coolingPeriod));
-            rewardGenerateTime = block.timestamp - stakingTime[msg.sender];
-            claimableReward[msg.sender] = _rewardPerSecond.mul(
-                rewardGenerateTime
-            );
-     
-            rewardGenerateTime = claimableReward[msg.sender].div(_rewardPerSecond);
-            claimableReward[msg.sender] = _rewardPerSecond.mul(
-                rewardGenerateTime
-            );
-       
-        emit rewardGenerated(
-            msg.sender,
-            _totalReward,
-            _rewardPerSecond,
-            rewardGenerateTime
-        );
-    }
 }
-
-
